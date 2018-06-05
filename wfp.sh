3ac175ca936ea246d2cb6223b59c41ce066dc6f0
steveharsant <38461965+steveharsant@users.noreply.github.com>
2018-06-05 14:29:05 +0100

    Update wfp.sh

--- wfp.sh
+++ wfp.sh
@@ -4,7 +4,7 @@
 #                 WFP: WiFi Password                #
 #               Author: Steven Harsant              #
 #                  Date: 5/6/2018                   #
-#                   Version: 1.0                    #
+#                   Version: 1.1                    #
 #===================================================#
 
 #Set Colour Variables For Output#
@@ -20,6 +20,44 @@ PASS=${GREEN}'PASS:'${WHITE} #PASS MESSAGES
 INFO=${YELLOW}'INFO:'${WHITE} #INFO MESSAGES
 HINT=${BLUE}'HINT:'${WHITE} #HINT MESSAGES
 
+#Get Script Path#
+#---------------#
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPTNAME=`basename "$0"`
+
+#If No Arguements Are Passed Output Info#
+#---------------------------------------#
+if [[ -z $1 ]]; then
+  printf "WiFi Password Extractor \n"
+  printf "Usage: wfp [SSID NAME] \n"
+
+  if [[ ! -f /usr/bin/wfp ]]; then
+    printf "First run prompts to be installed to /usr/bin/ \n"
+  fi
+
+  exit
+fi
+
+#Check If Installed and Install Itself#
+#-------------------------------------#
+if [[ ! -f /usr/bin/wfp ]]; then
+  printf "${INFO} wfp not installed. Would you like to install this script? \n"
+
+  while true; do
+      read -p "Continue [y/n]?" YN
+      case $YN in
+          [Yy]* ) sudo cp ${SCRIPTPATH}/${SCRIPTNAME} /usr/bin/wfp
+                  sudo chmod +x /usr/bin/wfp
+                  break;;
+          [Nn]* ) break;;
+          * ) echo "Please answer yes or no";;
+      esac
+  done
+
+fi
+
+#Check For Config and Display Password#
+#-------------------------------------#
 if [[ -f /etc/NetworkManager/system-connections/$1 ]]
   then
     PSK=`sudo cat /etc/NetworkManager/system-connections/$1 | grep psk= | sed 's/^.\{4\}//g'`

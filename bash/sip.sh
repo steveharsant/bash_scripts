7b185c6bfac59c7bdf58bc7d003180ae29f9fb2b
Steve Harsant <38461965+steveharsant@users.noreply.github.com>
2019-09-26 12:24:29 +0100

    Variables to uppercase

--- bash/sip.sh
+++ bash/sip.sh
@@ -6,12 +6,12 @@
 # Get IP address and loop through output
 ip a s  | while read -r line ; do
   # If the line starts with a digit [0-9], get contents between : characters
-  if [[ $line =~ ^[0-9] ]];then
-    INT=$(grep -oP '(?<=:).*?(?=:)' <<< "$line" | tr -d ' ')
+  if [[ $LINE =~ ^[0-9] ]];then
+    INT=$(grep -oP '(?<=:).*?(?=:)' <<< "$LINE" | tr -d ' ')
   fi
   # If the line contains 'inet', get matching contents
-  if [[ $line =~ 'inet' ]]; then
-    IP=$(grep -oP '(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\/\d{1,2})' <<< "$line")
+  if [[ $LINE =~ 'inet' ]]; then
+    IP=$(grep -oP '(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\/\d{1,2})' <<< "$LINE")
     # If an IP address is stored as a variable, echo the interface and IP address
     if [[ -n $IP ]]; then
       echo "$INT: $IP"
